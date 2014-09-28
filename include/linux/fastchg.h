d3e8aef89b52b7146f6fb4c9b4a537afee5aca8b
Rémi Cohen-Scali <Rmi Cohen-Scali remi.cohen-scali@laposte.net>
2014-09-28 21:23:09 +0200

    y-cable charging faux123

--- include/linux/fastchg.h
+++ include/linux/fastchg.h
@@ -12,10 +12,12 @@
  *
  */
 
-
 #ifndef _LINUX_FASTCHG_H
 #define _LINUX_FASTCHG_H
 
 extern int force_fast_charge;
+
+#define FAST_CHARGE_DISABLED 0	/* default */
+#define FAST_CHARGE_FORCE_AC 1
 
 #endif
